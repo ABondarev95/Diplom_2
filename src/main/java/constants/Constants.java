61997f95ae2ad136e7b980c3d3a0193f72e9fb63
Aleksandr Bondarev <alexandr.s.bondarev@yandex.ru>
2023-05-12 20:13:23 +0600

    add order model and order tests

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
     public static final String USER_API_CREATE = "/api/auth/register";
     public static final String USER_API_LOGIN = "/api/auth/login";
     public static final String USER_API = "/api/auth/user";
-
+    public static final String ORDER_API = "/api/orders";
+    public static final String ORDER_API_INGREDIENTS = "api/ingredients";
 }
